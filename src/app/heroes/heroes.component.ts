d32c49807c119ce2dc60e8be0c91caf1d3efd1f8
gmbroth <garry.m.brother@gmail.com>
2022-05-06 16:41:29 -0400

    Reject duplicate heroes

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,13 @@
+import { Injectable } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
 
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+
+@Injectable({
+  providedIn: 'root'
+})
 
 @Component({
   selector: 'app-heroes',
@@ -10,17 +16,35 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
 
-  heroes: Hero[] = [];
+  errors: string[] = [];  // error messages
+  heroes: Hero[]   = [];
 
-  constructor(private heroService: HeroService) { }
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
 
   ngOnInit(): void {
     this.getHeroes();
   }
 
+  clearErrors(): void {
+    this.errors = [];
+  }
+
+  addError(message: string): void {
+    this.errors.push(message);
+  }
+
+  getErrors(): string[] {
+    return this.errors;
+  }
+
   add(name: string): void {
     name = name.trim();
     if (!name) { return; }
+    if (this.heroes.some(element => element.name === name)) {
+      this.addError(`A hero with the name "${name}" already exists; please try a different name`);
+      this.messageService.add(`Rejected duplicate hero name ${name}`);
+      return;
+    }
     this.heroService.addHero({name} as Hero).subscribe(hero => {this.heroes.push(hero);});
   }
 
